36ca59aef1055e00590e0fcd60a3fd0faf179abc
Mike Tagg <mdtagg91@gmail.com>
2022-09-26 15:17:55 -0700

    nothing changed from last commit, just practice

--- index.js
+++ index.js
@@ -3,34 +3,15 @@ const parentContainer = document.querySelector('.container')
 
 function gridCreator(gridBlocks) {
     const totalBlocks = gridBlocks * gridBlocks
+    const blockWidth = 100 / gridBlocks
+
     for(let i = 0;i < totalBlocks;i++) {
         const blocks = document.createElement('div');
         blocks.classList.add('block')
         parentContainer.appendChild(blocks)
     }
-
-    // blockCreator(16)
 }
 
 gridCreator(16)
 
-function blockCreator() {
-
-    const blocks = 16;
-    const rowBlock = document.querySelectorAll('.row-container')
-
-    for(let j = 0;j < blocks;j++) {
-        for(let i = 0;i < blocks;i++) {
-            const block = document.createElement('div')
-            block.classList.add('block')
-            rowBlock[i].appendChild(block)
-        }
-    }
-}
-
-// const firstDiv = document.createElement('div')
-// firstDiv.classList.add('row-container')
-
-// const parentContainer = document.querySelector('.container')
-// parentContainer.appendChild(firstDiv)
 
