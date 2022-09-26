b2270a51bd47bd367fba0c839f47c0c6e5ef9d8f
Mike Tagg <mdtagg91@gmail.com>
2022-09-26 12:53:26 -0700

    row creator function created to programmatically create rows
    This function is going to be used to dynamically create the grid for the user based on different inputs

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const firstDiv = document.createElement('div')
-firstDiv.classList.add('row-container')
 
-const parentContainer = document.querySelector('.container')
-parentContainer.appendChild(firstDiv)
+function rowCreator() {
+    const rows = 4;
+    const parentContainer = document.querySelector('.container')
+    
+    for(let i = 0;i < rows;i++) {
+        const row = document.createElement('div');
+        row.classList.add('row-container')
+        
+        parentContainer.appendChild(row)
+    }
+
+}
+
+rowCreator()
+
+// const firstDiv = document.createElement('div')
+// firstDiv.classList.add('row-container')
+
+// const parentContainer = document.querySelector('.container')
+// parentContainer.appendChild(firstDiv)
+
