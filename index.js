89a6f1f7844ca55881d379e4ff65c7002c24c30c
Mike Tagg <mdtagg91@gmail.com>
2022-09-26 14:24:22 -0700

    block creator function created, border styling applied to blocks
    The block creator function is used to create the blocks in each of the rows to respond to different user requirments

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 
+const parentContainer = document.querySelector('.container')
+
 function rowCreator() {
     const rows = 4;
-    const parentContainer = document.querySelector('.container')
     
     for(let i = 0;i < rows;i++) {
         const row = document.createElement('div');
         row.classList.add('row-container')
-        
         parentContainer.appendChild(row)
     }
 
+    blockCreator()
 }
 
 rowCreator()
+
+function blockCreator() {
+
+    const blocks = 4;
+    const rowBlock = document.querySelectorAll('.row-container')
+
+    for(let j = 0;j < blocks;j++) {
+        for(let i = 0;i < blocks;i++) {
+            const block = document.createElement('div')
+            block.classList.add('block')
+            rowBlock[i].appendChild(block)
+        }
+    }
+}
 
 // const firstDiv = document.createElement('div')
 // firstDiv.classList.add('row-container')
