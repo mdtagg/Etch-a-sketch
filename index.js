8a09d16bfaf2c9e0044af4fece6bdb8e75775679
Mike Tagg <mdtagg91@gmail.com>
2022-09-26 12:35:58 -0700

    Added first div to container div, added a background color of blue with height and width as a visual aid

---
+++ index.js
@@ -0,0 +1,5 @@
+const firstDiv = document.createElement('div')
+firstDiv.classList.add('row-container')
+
+const parentContainer = document.querySelector('.container')
+parentContainer.appendChild(firstDiv)
